8a55fcdf63a531a3409846dfe625ae869312f629
jjshoots <taijunjet@hotmail.com>
2023-03-17 17:41:23 +0000

    first commit

---
+++ spotify_autoplay.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+from __future__ import annotations
+
+import time
+
+import os
+import dotenv
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+_THIS_DEVICE_NAME_ = "Pi Spotify"
+_CHECK_INTERVAL_ = 900
+_TIMEOUT_TO_TAKEOVER_ = 1800
+
+class Spotify:
+    def __init__(self, this_device_name):
+        self.spotify = spotipy.Spotify(
+            auth_manager=SpotifyOAuth(
+                client_id=os.environ.get("ID"),
+                client_secret=os.environ.get("SECRET"),
+                redirect_uri="http://localhost:8888/callback",
+                scope="streaming,user-modify-playback-state,user-read-playback-state,user-read-currently-playing,user-library-read",
+            )
+        )
+
+        # constants
+        self.this_device_name = this_device_name
+
+    def get_this_device(self) -> dict | None:
+        try:
+            devices = self.spotify.devices()
+        except Exception as e:
+            devices = None
+            print(e)
+
+        # something went wrong here
+        if devices is None:
+            return None
+
+        # returns a string for this device
+        all_devices = devices["devices"]
+
+        for device in all_devices:
+            if device["name"].lower() == self.this_device_name.lower():
+                return device
+
+        # we're not supposed to end up here
+        print("This device not on available devices, likely Raspotify failed!")
+        return None
+
+    def update_current_playback(self):
+        # get the currently playing song
+        self.current_playback = self.spotify.current_playback()
+
+    def is_playing(self) -> bool:
+        # no playback info means no song played for awhile
+        if self.current_playback is None:
+            return False
+
+        # check whether we're really playing
+        return self.current_playback.get("is_playing")
+
+    def play(self):
+        this_device = self.get_this_device()
+
+        print("Attempting to play...")
+
+        # check that we actually have this device
+        if this_device is None:
+            print("Failed to play: device not found!")
+            return
+
+        self.spotify.volume(0, device_id=this_device["id"])
+        self.spotify.transfer_playback(device_id=this_device["id"], force_play=True)
+
+
+if __name__ == "__main__":
+
+    # load spotify credentials
+    dotenv_path = os.path.join(os.path.dirname(__file__), "credentials")
+    dotenv.load_dotenv(dotenv_path)
+
+    # initiate the client
+    client = Spotify(_THIS_DEVICE_NAME_)
+
+    # takeover on restart minus 2 intervals, so we don't immediately take over on reset
+    total_timeout = _TIMEOUT_TO_TAKEOVER_ - _CHECK_INTERVAL_ * 2
+
+    while True:
+        # sleep a bit
+        time.sleep(_CHECK_INTERVAL_)
+
+        # update our status
+        client.update_current_playback()
+
+        # if not playing, continue waiting until play
+        if client.is_playing():
+            total_timeout = 0
+            continue
+
+        # if not playing for more than the takeover time, play
+        if total_timeout >= _TIMEOUT_TO_TAKEOVER_:
+            client.play()
+            continue
+
+        total_timeout += _CHECK_INTERVAL_
+        print(f"Haven't been playing for {total_timeout} seconds.")
---
+++ spotify_true_shuffle.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+from __future__ import annotations
+
+import random
+import time
+
+import os
+import dotenv
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+_CHECK_INTERVAL_ = 180
+_MAIN_PLAYLIST_ID_ = "6LCRCv7hmovwG9hzlqZI7V"
+
+
+class Spotify:
+    def __init__(self, main_playlist_id):
+        self.spotify = spotipy.Spotify(
+            auth_manager=SpotifyOAuth(
+                client_id="9497b0de1e2f41e494ae5fcf1d73776a",
+                client_secret="dc4e81242daa458aabd21dd5f26981fc",
+                redirect_uri="http://localhost:8888/callback",
+                scope="streaming,user-modify-playback-state,user-read-playback-state,user-read-currently-playing,user-library-read",
+            )
+        )
+
+        # constants
+        self.main_playlist_id = main_playlist_id
+        self.all_song_uris = self.get_all_uris_from_playlist(main_playlist_id)
+
+        # runtime variables
+        self.queued_song = "null"
+        self.current_playback = self.spotify.current_playback()
+
+        print(f"Initialized with {len(self.all_song_uris)} songs.")
+
+    def get_all_uris_from_playlist(self, playlist_id) -> list[str]:
+        # returns all uris in the playlist as a list of string
+        tracks_response = self.spotify.playlist_tracks(playlist_id)
+        uris = [track["track"]["uri"] for track in tracks_response["items"]]
+        while tracks_response["next"]:
+            tracks_response = self.spotify.next(tracks_response)
+            uris.extend([track["track"]["uri"] for track in tracks_response["items"]])
+
+        return uris
+
+    def update_current_playback(self):
+        # get the currently playing song
+        self.current_playback = self.spotify.current_playback()
+
+    def is_playing(self) -> bool:
+        # no playback info means no song played for awhile
+        if self.current_playback is None:
+            return False
+
+        # check whether we're really playing
+        return self.current_playback.get("is_playing")
+
+    def playback_is_playlist(self) -> bool:
+        if self.current_playback is None:
+            return False
+
+        return (
+            self.current_playback["context"]["uri"].split(":")[-1]
+            == self.main_playlist_id
+        )
+
+    def check_add_queue(self):
+        try:
+            queue = client.spotify.queue()
+        except Exception as e:
+            queue = None
+            print(e)
+
+        # something went wrong here
+        if queue is None:
+            return
+
+        all_queued_uris = [track["uri"] for track in queue["queue"]]
+
+        # if the queued song is not what we queued, add a new one to queue
+        if self.queued_song in all_queued_uris:
+            print("Queued song already in queue, skipping add...")
+            return
+
+        print("Adding to queue...")
+
+        # queues one song and stores the queued song
+        self.queued_song = random.choice(self.all_song_uris)
+        self.spotify.add_to_queue(self.queued_song)
+
+
+if __name__ == "__main__":
+    # load spotify credentials
+    dotenv_path = os.path.join(os.path.dirname(__file__), "credentials")
+    dotenv.load_dotenv(dotenv_path)
+
+    # initiate the client
+    client = Spotify(_MAIN_PLAYLIST_ID_)
+
+    while True:
+        # sleep a bit
+        time.sleep(_CHECK_INTERVAL_)
+
+        # update our status
+        client.update_current_playback()
+
+        # override the queue if conditions are right
+        if client.is_playing() and client.playback_is_playlist():
+            print("Playlist being played, checking queue...")
+            client.check_add_queue()
+        else:
+            print("Not playing ATM, waiting...")
