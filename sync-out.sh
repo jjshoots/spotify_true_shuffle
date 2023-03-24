97f3cc2fc392123b31e8ffee0252028463e69ed5
jjshoots <taijunjet@hotmail.com>
2023-03-24 13:00:14 +0000

    fix sync script

--- sync-out.sh
+++ sync-out.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -rav --progress ./credentials pi@192.168.0.180:~/spotify_scripts/credentials
+rsync -rav --progress ./credentials/ pi@192.168.0.180:~/spotify_scripts/credentials
